59ec548068883316129e8a2a7e0e14ce986c5cce
J Carlos Viana F <jcvf@ic.ufal.br>
2016-06-09 17:04:51 -0300

    begin to fill InfoFile constructor

--- src/util/InfoFile.java
+++ src/util/InfoFile.java
@@ -1,5 +1,10 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,12 +15,26 @@ import repository.Repository;
 
 public class InfoFile {
 
+    private static final String INFOFILE_PATH = "infofile";
+
     private static InfoFile infoFile = null;
 
     private Map<Repository, List<AnalysisUnit>> repoMap = new HashMap<Repository, List<AnalysisUnit>>();
 
     private InfoFile() {
-        
+        File file = new File(INFOFILE_PATH);
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            
+            for(String line; (line = bufferedReader.readLine()) != null; ) {
+                // TODO process the line
+            }
+        } catch (IOException e) {
+            System.out.println("error to read infofile");
+        }
+
         // TODO build repoMap
     }
 
